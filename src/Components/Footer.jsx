05b406e757422e7d30b925065373d006b22df0e7
OctavioCarri <octavio.carrizo2003@gmail.com>
2023-10-18 23:10:36 -0300

    Iniciando El Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Footer.css'
+
+const Footer=() => {
+    return(
+     <>
+        <div className="footer">
+            <div className="sb_footer section_padding">
+                 <div className="sb_footer-links-div">
+                    <h4>for business</h4>
+                    <a href="/employer">
+                        <p></p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </>
+    )
+}
