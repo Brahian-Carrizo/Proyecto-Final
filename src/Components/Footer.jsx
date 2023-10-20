11f80f8e442fd5b9716ebe9dd7f80d1dd3923148
OctavioCarri <octavio.carrizo2003@gmail.com>
2023-10-20 15:42:29 -0300

    Footer en Proceso Cargado de Imagenes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,20 +1,67 @@
 import React from "react";
 import './Footer.css'
+import fb from "../Img/Logo-Facebook.png"
+import twitter from "../Img/Logo-Twitter.png"
+import insta from "../Img/Logo-Instagram.webp"
+import linkedin from "../Img/Logo-Linkedin.png"
 
-const Footer=() => {
-    return(
-     <>
-        <div className="footer">
-            <div className="sb_footer section_padding">
-                 <div className="sb_footer-links-div">
-                    <h4>for business</h4>
-                    <a href="/employer">
-                        <p></p>
-                    </a>
+const Footer = () => {
+    return (
+        <>
+            <div className="footer">
+                <div className="sb_footer section_padding">
+                    <div className="sb_footer-links">
+                        <div className="sb_footer-links-div">
+                            <h4>for business</h4>
+                            <a href="/employer">
+                                <p>Employer</p>
+                            </a>
+                            <a href="/Health Plan">
+                                <p>Health Plan</p>
+                            </a>
+                            <a href="/individual">
+                                <p>individual</p>
+                            </a>
+                            <div className="sb_fotter-links_div">
+                                <h4>for business</h4>
+                                <a href="/resource">
+                                    <p>Resource center</p>
+                                </a>
+                                <a href="/resource">
+                                    <p>Testimonials</p>
+                                </a>
+                                <a href="/resource">
+                                    <p>STV</p>
+                                </a>
+                            </div>
+                            <div className="sb_footer-links_div">
+                                <h4>Partners</h4>
+                                <a href="/about">
+                                    <p>About</p>
+                                </a>
+                                <a href="/Press">
+                                    <p>Press</p>
+                                </a>
+                                <a href="/career">
+                                    <p>Contact</p>
+                                </a>
+                            </div>
+                            <div className="sb_footer-links_div">
+                                <h4>Coming soon on</h4>
+                                <div className="socialmedia">
+                                    <p><img src={fb} alt="" /></p>
+                                    <p><img src={twitter} alt="" /></p>
+                                    <p><img src={linkedin} alt="" /></p>
+                                    <p><img src={insta} alt="" /></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
 
-    </>
+        </>
     )
 }
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,11 @@ import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Navbar from "./Components/Navbar.jsx"
+import Footer from './Components/Footer.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Navbar/>   
+ReactDOM.createRoot(document.getElementById('root')).render(    
+    <>
+        <Navbar/>
+        <Footer/>
+    </>
 )
