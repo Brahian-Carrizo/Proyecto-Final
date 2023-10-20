825d65d1092562e04a401811f987602389bea3c7
OctavioCarri <octavio.carrizo2003@gmail.com>
2023-10-20 16:00:55 -0300

    Terminado (Html del footer)

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,60 +7,74 @@ import linkedin from "../Img/Logo-Linkedin.png"
 
 const Footer = () => {
     return (
-        <>
-            <div className="footer">
-                <div className="sb_footer section_padding">
-                    <div className="sb_footer-links">
-                        <div className="sb_footer-links-div">
+        <div className="footer">
+            <div className="sb_footer section_padding">
+                <div className="sb_footer-links">
+                    <div className="sb_footer-links-div">
+                        <h4>for business</h4>
+                        <a href="/employer">
+                            <p>Employer</p>
+                        </a>
+                        <a href="/Health Plan">
+                            <p>Health Plan</p>
+                        </a>
+                        <a href="/individual">
+                            <p>individual</p>
+                        </a>
+                        <div className="sb_fotter-links_div">
                             <h4>for business</h4>
-                            <a href="/employer">
-                                <p>Employer</p>
+                            <a href="/resource">
+                                <p>Resource center</p>
                             </a>
-                            <a href="/Health Plan">
-                                <p>Health Plan</p>
+                            <a href="/resource">
+                                <p>Testimonials</p>
                             </a>
-                            <a href="/individual">
-                                <p>individual</p>
+                            <a href="/resource">
+                                <p>STV</p>
                             </a>
-                            <div className="sb_fotter-links_div">
-                                <h4>for business</h4>
-                                <a href="/resource">
-                                    <p>Resource center</p>
-                                </a>
-                                <a href="/resource">
-                                    <p>Testimonials</p>
-                                </a>
-                                <a href="/resource">
-                                    <p>STV</p>
-                                </a>
-                            </div>
-                            <div className="sb_footer-links_div">
-                                <h4>Partners</h4>
-                                <a href="/about">
-                                    <p>About</p>
-                                </a>
-                                <a href="/Press">
-                                    <p>Press</p>
-                                </a>
-                                <a href="/career">
-                                    <p>Contact</p>
-                                </a>
-                            </div>
-                            <div className="sb_footer-links_div">
-                                <h4>Coming soon on</h4>
-                                <div className="socialmedia">
-                                    <p><img src={fb} alt="" /></p>
-                                    <p><img src={twitter} alt="" /></p>
-                                    <p><img src={linkedin} alt="" /></p>
-                                    <p><img src={insta} alt="" /></p>
-                                </div>
+                        </div>
+                        <div className="sb_footer-links_div">
+                            <h4>Partners</h4>
+                            <a href="/about">
+                                <p>About</p>
+                            </a>
+                            <a href="/Press">
+                                <p>Press</p>
+                            </a>
+                            <a href="/career">
+                                <p>Contact</p>
+                            </a>
+                        </div>
+                        <div className="sb_footer-links_div">
+                            <h4>Coming soon on</h4>
+                            <div className="socialmedia">
+                                <p><img src={fb} alt="" /></p>
+                                <p><img src={twitter} alt="" /></p>
+                                <p><img src={linkedin} alt="" /></p>
+                                <p><img src={insta} alt="" /></p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-        </>
+                <hr></hr>
+
+                <div className="sb_footer-below">
+                    <div className="sb_footer-copyright">
+                        <p>
+                            @{new Date().getFullYear()} codeinn. all right reserved.
+                        </p>
+                    </div>
+                    <div className="sb_footer-below-links">
+                        <a href="/terms"><div><p>Terms & conditions</p></div></a>
+                        <a href="/privacy"><div><p>Privacy</p></div></a>
+                        <a href="/security"><div><p>Security</p></div></a>
+                        <a href="/cookie"><div><p>Cookie Declarations</p></div></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
